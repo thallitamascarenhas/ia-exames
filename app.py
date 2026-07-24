13364715345e86ef9a956fff9e752f2f5f52c83b
thallitamascarenhas <thallitasamantha@gmail.com>
2026-07-24 11:32:32 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="IA Exames",
+    page_icon="🩺",
+    layout="wide"
+)
+
+st.title("🩺 IA Exames")
+
+st.subheader("Bem-vinda!")
+
+st.write("Projeto em desenvolvimento.")
