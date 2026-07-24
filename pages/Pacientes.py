e91c21a60d3fd03de2608bb7c5e000a882e19fd6
thallitamascarenhas <thallitasamantha@gmail.com>
2026-07-24 14:33:53 -0300

    Criar cadastro de pacientes.py

--- pages/Pacientes.py
+++ pages/Pacientes.py
@@ -1,17 +1,106 @@
 import streamlit as st
 
-st.set_page_config(page_title="Pacientes", page_icon="👥")
+from database.pacientes import (
+    salvar_paciente,
+    listar_pacientes
+)
 
-st.title("👥 Pacientes")
 
-st.write("Cadastro de pacientes")
+st.set_page_config(
+    page_title="Pacientes",
+    page_icon="👥"
+)
 
-import streamlit as st
 
-from database.supabase import listar_pacientes
+st.title("👥 Cadastro de Pacientes")
 
-st.title("👥 Pacientes")
+
+with st.form("form_paciente"):
+
+    nome = st.text_input(
+        "Nome completo"
+    )
+
+
+    nascimento = st.date_input(
+        "Data de nascimento"
+    )
+
+
+    sexo = st.selectbox(
+        "Sexo",
+        [
+            "Feminino",
+            "Masculino",
+            "Outro"
+        ]
+    )
+
+
+    altura = st.number_input(
+        "Altura (cm)",
+        min_value=0
+    )
+
+
+    peso = st.number_input(
+        "Peso (kg)",
+        min_value=0.0
+    )
+
+
+    telefone = st.text_input(
+        "Telefone"
+    )
+
+
+    email = st.text_input(
+        "Email"
+    )
+
+
+    observacoes = st.text_area(
+        "Observações"
+    )
+
+
+    salvar = st.form_submit_button(
+        "💾 Salvar paciente"
+    )
+
+
+    if salvar:
+
+        salvar_paciente(
+            nome,
+            nascimento.isoformat(),
+            sexo,
+            altura,
+            peso,
+            telefone,
+            email,
+            observacoes
+        )
+
+
+        st.success(
+            "Paciente cadastrado com sucesso!"
+        )
+
+
+
+st.divider()
+
+
+st.subheader(
+    "Pacientes cadastrados"
+)
+
 
 dados = listar_pacientes()
 
-st.write(dados)
+
+st.dataframe(
+    dados,
+    use_container_width=True
+)
