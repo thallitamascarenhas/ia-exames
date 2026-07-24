20b6914a9e99b317347089b805fc463b5e955e97
thallitamascarenhas <thallitasamantha@gmail.com>
2026-07-24 13:51:56 -0300

    Update Pacientes.py

--- pages/Pacientes.py
+++ pages/Pacientes.py
@@ -5,3 +5,13 @@ st.set_page_config(page_title="Pacientes", page_icon="👥")
 st.title("👥 Pacientes")
 
 st.write("Cadastro de pacientes")
+
+import streamlit as st
+
+from database.supabase import listar_pacientes
+
+st.title("👥 Pacientes")
+
+dados = listar_pacientes()
+
+st.write(dados)
