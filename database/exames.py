aa3d5cee6987721b3e7eaf9b12be02508e6e6aea
thallitamascarenhas <thallitasamantha@gmail.com>
2026-07-24 15:51:39 -0300

    Create exames.py

---
+++ database/exames.py
@@ -0,0 +1,41 @@
+from database.supabase import supabase
+
+
+def listar_exames():
+
+    resposta = (
+        supabase
+        .table("exames")
+        .select("*")
+        .execute()
+    )
+
+    return resposta.data
+
+
+
+def salvar_exame(
+    paciente_id,
+    data_exame,
+    laboratorio,
+    nome_pdf
+):
+
+    dados = {
+
+        "paciente_id": paciente_id,
+        "data_exame": data_exame,
+        "laboratorio": laboratorio,
+        "nome_pdf": nome_pdf
+
+    }
+
+
+    resposta = (
+        supabase
+        .table("exames")
+        .insert(dados)
+        .execute()
+    )
+
+    return resposta.data
