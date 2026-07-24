f006f2c425dad21f8e66af1d52e0b42a6e73218c
thallitamascarenhas <thallitasamantha@gmail.com>
2026-07-24 13:50:21 -0300

    Create supabase.py

---
+++ database/supabase.py
@@ -0,0 +1,11 @@
+from supabase import create_client
+import streamlit as st
+
+@st.cache_resource
+def conectar():
+
+    url = st.secrets["SUPABASE_URL"]
+
+    key = st.secrets["SUPABASE_KEY"]
+
+    return create_client(url, key)
