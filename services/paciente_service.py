2ba323bfc7dec5ab2230ec1c529a4b060dc19b49
thallitamascarenhas <thallitasamantha@gmail.com>
2026-07-24 15:27:25 -0300

    Create paciente_service.py

---
+++ services/paciente_service.py
@@ -0,0 +1,24 @@
+from database.pacientes import (
+    salvar_paciente,
+    listar_pacientes
+)
+
+
+def cadastrar_paciente(dados):
+
+    return salvar_paciente(
+        dados["nome"],
+        dados["nascimento"],
+        dados["sexo"],
+        dados["altura"],
+        dados["peso"],
+        dados["telefone"],
+        dados["email"],
+        dados["observacoes"]
+    )
+
+
+
+def buscar_pacientes():
+
+    return listar_pacientes()
