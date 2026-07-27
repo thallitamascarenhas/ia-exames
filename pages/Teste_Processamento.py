e09c7be8338002b87379ad59f2931f2900a005cd
thallitamascarenhas <thallitasamantha@gmail.com>
2026-07-27 08:56:18 -0300

    Create Teste_Processamento.py

---
+++ pages/Teste_Processamento.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+from services.pdf_service import extrair_texto_pdf
+from services.resultado_service import encontrar_marcadores
+
+
+st.title("🔎 Processamento de exame")
+
+
+arquivo = st.file_uploader(
+    "Enviar PDF",
+    type=["pdf"]
+)
+
+
+if arquivo:
+
+
+    texto = extrair_texto_pdf(
+        arquivo
+    )
+
+
+    st.subheader("Texto extraído")
+
+    st.text(texto)
+
+
+    encontrados = encontrar_marcadores(
+        texto
+    )
+
+
+    st.subheader("Marcadores encontrados")
+
+
+    st.write(
+        encontrados
+    )
