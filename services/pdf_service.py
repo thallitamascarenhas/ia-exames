6a96e44d249377ed524e671d1015aab13842a338
thallitamascarenhas <thallitasamantha@gmail.com>
2026-07-27 10:58:02 -0300

    Update pdf_service.py

--- services/pdf_service.py
+++ services/pdf_service.py
@@ -3,16 +3,27 @@ import fitz
 
 def extrair_texto_pdf(arquivo_pdf):
 
+    if hasattr(arquivo_pdf, "read"):
+
+        arquivo = arquivo_pdf.read()
+
+    else:
+
+        arquivo = arquivo_pdf
+
+
     documento = fitz.open(
-        stream=arquivo_pdf.read(),
+        stream=arquivo,
         filetype="pdf"
     )
 
+
     texto = ""
 
+
     for pagina in documento:
+
         texto += pagina.get_text()
 
-    documento.close()
 
     return texto
