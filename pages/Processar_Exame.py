7c816a555fb0059ee6f53071a30668623807700a
thallitamascarenhas <thallitasamantha@gmail.com>
2026-07-27 11:17:49 -0300

    Update Processar_Exame.py

--- pages/Processar_Exame.py
+++ pages/Processar_Exame.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
-from services.exame_service import buscar_exames, obter_pdf_exame
-from services.pdf_service import extrair_texto_pdf
+from services.exame_service import (
+    buscar_exames,
+    obter_pdf_exame
+)
+
+from services.pdf_service import (
+    extrair_texto_pdf
+)
+
+from services.resultado_service import (
+    processar_resultados_exame
+)
 
 
 st.title("🔬 Processar exame")
@@ -10,7 +20,13 @@ st.title("🔬 Processar exame")
 exames = buscar_exames()
 
 
-if exames:
+if not exames:
+
+    st.warning(
+        "Nenhum exame cadastrado."
+    )
+
+else:
 
     opcoes = {
         exame["id"]: exame
@@ -55,26 +71,19 @@ if exames:
             texto
         )
 
-from services.resultado_service import processar_resultados_exame
+
+        resultados = processar_resultados_exame(
+            exame["id"],
+            texto
+        )
 
 
-resultados = processar_resultados_exame(
-    exame["id"],
-    texto
-)
+        st.subheader(
+            "Resultados processados"
+        )
 
 
-st.subheader(
-    "Resultados processados"
-)
-
-
-st.dataframe(
-    resultados,
-    width="stretch"
-)
-else:
-
-    st.warning(
-        "Nenhum exame cadastrado."
-    )
+        st.dataframe(
+            resultados,
+            width="stretch"
+        )
